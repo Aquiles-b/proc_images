4dbddf2f7c08f219c93abfb60e6ae03f64624e89
Aquiles-b <brunoaquiles@ufpr.br>
2024-03-24 22:24:57 -0300

    [ADD] teste

--- t2/floresta.py
+++ t2/floresta.py
@@ -1,15 +1,32 @@
 import cv2
 import sys
+import numpy as np
 
 
 def main() -> None:
     img_in_name = sys.argv[1]
     img_out_name = sys.argv[2]
 
-    img = cv2.imread(img_in_name, cv2.COLOR_BGR2HSV)
+    img = cv2.imread(img_in_name)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    cv2.imshow('Teste', img)
-    cv2.waitKey(0)
+    mask = cv2.inRange(hsv, (0, 0, 0), (75, 255, 255))
+
+    imask = mask>0
+    orange=np.zeros_like(img,np.uint8)
+    orange[imask]=img[imask]
+    yellow=img.copy()
+    hsv[...,0] = hsv[...,0]+20
+    yellow[imask]=cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)[imask]
+    yellow=np.clip(yellow,0,255)
+
+    cv2.imshow("img",yellow)
+    cv2.waitKey()
+    nofish=img.copy()
+    nofish=cv2.bitwise_and(nofish,nofish,mask=(np.bitwise_not(imask)).astype(np.uint8))
+    cv2.imshow("img",nofish)
+    cv2.waitKey()
+
 
 
 if __name__ == "__main__":
