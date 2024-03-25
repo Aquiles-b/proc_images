1634a0298f05984b4d421fc8f34688aaca28c160
Aquiles-b <brunoaquiles@ufpr.br>
2024-03-25 09:54:13 -0300

    [ADD] mascara do canal g usada nas operações

--- t2/floresta.py
+++ t2/floresta.py
@@ -11,29 +11,32 @@ def main() -> None:
 
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    low_river = np.array([140 ,0 ,0])
-    high_river = np.array([213, 255, 255])
-
     low_forest = np.array([30, 0, 0])
-    high_forest = np.array([105, 255, 255])
+    high_forest = np.array([106, 255, 255])
 
     # Pega a mascara da floresta por range de cor
     mask_forest = cv2.inRange(hsv, low_forest, high_forest)
 
-    # Pega a mascara do rio por range de cor
-    mask_river = cv2.inRange(hsv, low_river, high_river)
-
-    # Pega a mascara do rio por canais
     b, g, r = cv2.split(img)
+    # Pega a mascara do rio por canais
     _, mask_river_b = cv2.threshold(b, 130, 255, cv2.THRESH_BINARY)
-    _, mask_river_r = cv2.threshold(r, 150, 255, cv2.THRESH_BINARY)
+    _, mask_river_r = cv2.threshold(r, 170, 255, cv2.THRESH_BINARY)
+
+    # Pega a mascara da floresta do canal g
+    _, mask_forest_g = cv2.threshold(g, 110, 255, cv2.THRESH_BINARY)
 
     # soma as mascaras dos rios
-    mask_river = cv2.bitwise_or(mask_river, mask_river_b)
-    mask_river = cv2.bitwise_or(mask_river, mask_river_r)
+    mask_river = cv2.bitwise_or(mask_river_b, mask_river_r)
 
     # subtrai a mascara do rio da mascara da floresta
     mask_forest = cv2.bitwise_and(mask_forest, cv2.bitwise_not(mask_river))
+
+    # subtrai a mascara b do rio da mascara g da floresta
+    mask_forest_g = cv2.bitwise_and(mask_forest_g, cv2.bitwise_not(mask_river_b))
+
+    # Soma a mascara do canal g com a mascara da floresta
+    mask_forest = cv2.bitwise_or(mask_forest, mask_forest_g)
+
     mask_forest = cv2.bitwise_not(mask_forest)
 
     img_seg = img.copy()
